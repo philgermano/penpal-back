f5c38dd1da74cc189e0b35c45516a8d980bb77de
pgermano <philipgermano@gmail.com>
2022-12-18 15:06:14 -0500

    basic schema

---
+++ models/pen.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const penSchema = new mongoose.Schema({
+    email:{
+        type:String, required: true, index: true
+    },
+    brand:{
+        type:String
+    },
+    model:{
+        type:String
+    },
+    nib:{
+        type:String
+    },
+    color:{
+        type:String
+    }
+    
+})
+
+const Pen = mongoose.model('Pen', penSchema)
+
+module.exports = Pen
